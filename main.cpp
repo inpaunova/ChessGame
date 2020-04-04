498ddbf871bf776888e9796866a0bd359597bb68
inpaunova <ipaunova24@gmail.com>
2020-04-04 23:20:54 +0300

    Modifications in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,33 +1,37 @@
 #include <iostream>
 #include <string>
-#include "chess.h"
+#include "ChessGame.h"
 
 using namespace std;
 
+void playGame(ChessGame game)
+{
+	bool wantNewGame = true;
+	string yesNoAnswer;
+	while (wantNewGame)
+	{
+		game.setGameBoard();
+		while (game.isGamePlaying)
+		{
+			game.playNextTurn();
+		}
+		cout << "Do you want to play again? (y for yes, anything else for no) ";
+		cin >> yesNoAnswer;
+		if (yesNoAnswer != "y")
+			wantNewGame = false;
+	}
+}
+
 int main()
 {
-	ChessGame b;
-	string s;
-	bool newgame = true;
+	ChessGame game;
+	string keyFromKeyBoard;
 	cout << "   _____ _    _ ______  _____ _____ \n  / ____| |  | |  ____|/ ____/ ____| \n | |    | |__| | |__  | (___| (___  \n | |    |  __  |  __|  \\___  \\___ \\ \n | |____| |  | | |____ ____) |___) | \n  \\_____|_|  |_|______|_____/_____/ \n" << endl;
 	cout << "A game by George Harter"<<endl;
 	cout << "Enter any key to continue" << endl;
-	cin >> s;
+	cin >> keyFromKeyBoard;
 
-	while(newgame)
-	{
-		
-		b.setGameBoard();
-		while (b.isGamePlaying)
-		{
-			b.playNextTurn();
-		}
-		cout << "Do you want to play again? (y for yes, anything else for no) ";
-		cin >> s;
-		if (s != "y")
-			newgame = false;
-	}
-
-
+	playGame(game);
+	
 	return 0;
 }
