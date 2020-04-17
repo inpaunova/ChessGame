e49af4dece63e4c1118af5ffad524e555c3864d7
inpaunova <ipaunova24@gmail.com>
2020-04-17 13:18:50 +0300

    Refactor class Rook for readability
    Add functions canMoveHorizontally, canMoveVertically and modify method
    canMove.

--- Rook.h
+++ Rook.h
@@ -3,42 +3,51 @@
 
 class Rook : public Figure
 {
-public:
-	virtual void printAsLetter(Color fieldColor)
+	bool canMoveHorizontally(Color ** fieldsColorsOfFiguresOnBoard, int sourceX, int sourceY, int destinationX, int destinationY)
 	{
-		(fieldColor == WHITE) ? cout << " R " : cout << " r ";
+		if (sourceX == destinationX && sourceY != destinationY)
+		{
+			int moveStep = (destinationY - sourceY) / (abs(destinationY - sourceY));
+			for (int i = sourceY + moveStep; i != destinationY; i += moveStep)
+			{
+				if (fieldsColorsOfFiguresOnBoard[destinationX][i] != NONE)
+					return false;
+			}
+			return true;
+		}
+		return false;
 	}
 
-	bool canMove(Color** fieldsColors, int sourceX, int sourceY, int destinationX, int destinationY)
+	bool canMoveVertically(Color ** fieldsColorsOfFiguresOnBoard, int sourceX, int sourceY, int destinationX, int destinationY)
+	{
+		if (sourceX != destinationY && sourceY == destinationY)
+		{
+			int moveStep = (destinationX - sourceX) / (abs(destinationX - sourceX));
+			for (int i = sourceX + moveStep; i != destinationX; i += moveStep)
+			{
+				if (fieldsColorsOfFiguresOnBoard[i][destinationY] != NONE)
+					return false;
+			}
+			return true;
+		}
+		return false;
+	}
+public:
+	virtual void printAsLetter(Color fieldColorOfFigure)
+	{
+		(fieldColorOfFigure == WHITE) ? cout << " R " : cout << " r ";
+	}
+
+	bool canMove(Color** fieldsColorsOfFiguresOnBoard, int sourceX, int sourceY, int destinationX, int destinationY)
 	{
 		//off board inputs should be handled elsewhere (before this)
 		//squares with same color should be handled elsewhere (before this)
-
-		if (sourceX != destinationX || sourceY != destinationY)
+		if (canMoveHorizontally(fieldsColorsOfFiguresOnBoard, sourceX, sourceY, destinationX, destinationY) ||
+			canMoveVertically(fieldsColorsOfFiguresOnBoard, sourceX, sourceY, destinationX, destinationY))
 		{
-			if (sourceX == destinationX)
-			{
-				int yIncrement = (destinationY - sourceY) / (abs(destinationY - sourceY));
-				for (int i = sourceY + yIncrement; i != destinationY; i += yIncrement)
-				{
-					if (fieldsColors[destinationX][i] != NONE)
-						return false;
-				}
-			}
-			else if (sourceY == destinationY)
-			{
-				int xIncrement = (destinationX - sourceX) / (abs(destinationX - sourceX));
-				for (int i = sourceX + xIncrement; i != destinationX; i += xIncrement)
-				{
-					if (fieldsColors[i][destinationY] != NONE)
-						return false;
-				}
-			}
-			else
-				return false;
+			return true;
 		}
-
-		return true;
+		return false;
 	}
 
 	virtual bool isKing()
