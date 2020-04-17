5faf897d257c0fa6423c6a606bf6577c8773118e
inpaunova <ipaunova24@gmail.com>
2020-04-17 12:09:27 +0300

    Change logic of King's movement
    Add functions to show if King can move horizontally or vertically and
    modify method canMove.

--- Figure.h
+++ Figure.h
@@ -1,11 +1,11 @@
 #pragma once
 
-enum Color { WHITE, BLACK, NONE };
+enum Colors { WHITE, BLACK, NONE };
 
 class Figure
 {
 public:
-	virtual void printAsLetter(Color fieldColor) = 0;
-	virtual bool canMove(Color ** fieldsColors, int sourceX, int sourceY, int destinationX, int destinationY) = 0;
+	virtual void printAsLetter(Colors fieldColorOfFigure) = 0;
+	virtual bool canMove(Colors ** fieldsColorsOfFiguresOnBoard, int sourceX, int sourceY, int destinationX, int destinationY) = 0;
 	virtual bool isKing() = 0;
 };
--- King.h
+++ King.h
@@ -3,23 +3,57 @@
 
 class King : public Figure
 {
-public:
-	virtual void printAsLetter(Color fieldColor)
+	bool canMoveHorizontally(sourceX, sourceY, destinationX, destinationY)
 	{
-		(fieldColor == WHITE) ? cout << " K " : cout << " k ";
-	}
-
-	virtual bool canMove(Color ** fieldsColors, int sourceX, int sourceY, int destinationX, int destinationY)
-	{
-		//off board inputs should be handled elsewhere (before this)
-		//squares with same color should be handled elsewhere (before this)
-		if (abs(destinationX - sourceX) == 1)
+		if (sourceX == destinationX)
 		{
 			if (abs(destinationY - sourceY) == 1)
 			{
 				return true;
 			}
-			else return false;
+		}
+		return false;
+	}
+
+	bool canMoveVertically(sourceX, sourceY, destinationX, destinationY)
+	{
+		if (sourceY == destinationY)
+		{
+			if (abs(destinationX - sourceX) == 1)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+
+	bool canMoveDiagonally(sourceX, sourceY, destinationX, destinationY)
+	{
+		if (abs(destinationX - sourceX) == 1)
+		{
+			if (abs(abs(destinationY - sourceY) == 1) == 1)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+
+public:
+	virtual void printAsLetter(Color fieldColorOfFigure)
+	{
+		(fieldColorOfFigure == WHITE) ? cout << " K " : cout << " k ";
+	}
+
+	virtual bool canMove(Color ** fieldsColorsOfFiguresOnBoard, int sourceX, int sourceY, int destinationX, int destinationY)
+	{
+		//off board inputs should be handled elsewhere (before this)
+		//squares with same color should be handled elsewhere (before this)
+		if (canMoveHorizontally(sourceX, sourceY, destinationX, destinationY) &&
+			canMoveVertically(sourceX, sourceY, destinationX, destinationY) &&
+			canMoveVertically(sourceX, sourceY, destinationX, destinationY))
+		{
+			return true;
 		}
 		return false;
 	}
