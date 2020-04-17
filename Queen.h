0fda82e4606cec21adcc381724ce32a8cae99683
inpaunova <ipaunova24@gmail.com>
2020-04-17 13:05:05 +0300

    Refactor class Queen for readability
    Modify method canMove by separating the logic into functions
    canMoveHorizontally, canMoveVertically, canMoveDiagonally.

--- Queen.h
+++ Queen.h
@@ -3,53 +3,71 @@
 
 class Queen : public Figure
 {
-public:
-	virtual void printAsLetter(Color fieldColor)
+	bool canMoveHorizontally(Color ** fieldsColorsOfFiguresOnBoard, int sourceX, int sourceY, int destinationX, int destinationY)
 	{
-		(fieldColor == WHITE) ? cout << " Q " : cout << " q ";
+		if (sourceX == destinationX && sourceY != destinationY)
+		{
+			int moveStep = (destinationY - sourceY) / (abs(destinationY - sourceY));
+			for (int i = sourceY + moveStep; i != destinationY; i += moveStep)
+			{
+				if (fieldsColorsOfFiguresOnBoard[destinationX][i] != NONE)
+					return false;
+			}
+			return true;
+		}
+		return false;
 	}
-	virtual bool canMove(Color ** fieldsColors, int sourceX, int sourceY, int destinationX, int destinationY)
+
+	bool canMoveVertically(Color ** fieldsColorsOfFiguresOnBoard, int sourceX, int sourceY, int destinationX, int destinationY)
+	{
+		if (sourceX != destinationY && sourceY == destinationY)
+		{
+			int moveStep = (destinationX - sourceX) / (abs(destinationX - sourceX));
+			for (int i = sourceX + moveStep; i != destinationX; i += moveStep)
+			{
+				if (fieldsColorsOfFiguresOnBoard[i][destinationY] != NONE)
+					return false;
+			}
+			return true;
+		}
+		return false;
+	}
+
+	bool canMoveDiagonally(Color ** fieldsColorsOfFiguresOnBoard, int sourceX, int sourceY, int destinationX, int destinationY)
+	{
+		if (abs(sourceX - destinationX) == abs(sourceY - destinationY))
+		{
+			int moveStepByX = (destinationX - sourceX) / (abs(destinationX - sourceX));
+			int moveStepByY = (destinationY - sourceY) / (abs(destinationY - sourceY));
+
+			int xCoordinate, yCoordinate;
+			for (int i = 1; i < abs(sourceX - destinationX); i++)
+			{
+				xCoordinate = sourceX + xIncrement * i;
+				yCoordinate = sourceY + yIncrement * i;
+				if (fieldsColorsOfFiguresOnBoard[xCoordinate][yCoordinate] != NONE)
+					return false;
+			}
+			return true;
+		}
+		return false;
+	}
+public:
+	virtual void printAsLetter(Color fieldColorOfFigure)
+	{
+		(fieldColorOfFIgure == WHITE) ? cout << " Q " : cout << " q ";
+	}
+	virtual bool canMove(Color ** fieldsColorsOfFiguresOnBoard, int sourceX, int sourceY, int destinationX, int destinationY)
 	{
 		//off board inputs should be handled elsewhere (before this)
 		//squares with same color should be handled elsewhere (before this)
-		int yIncrement;
-		int xIncrement;
-		if (sourceX != destinationX || sourceY != destinationY)
+		if (canMoveHorizontally(fieldsColorsOfFiguresOnBoard, sourceX, sourceY, destinationX, destinationY) ||
+			canMoveVertically(fieldsColorsOfFiguresOnBoard, sourceX, sourceY, destinationX, destinationY) ||
+			canMoveVertically(fieldsColorsOfFiguresOnBoard, sourceX, sourceY, destinationX, destinationY))
 		{
-			if (sourceX == destinationX)
-			{
-				yIncrement = (destinationY - sourceY) / (abs(destinationY - sourceY));
-				for (int i = sourceY + yIncrement; i != destinationY; i += yIncrement)
-				{
-					if (fieldsColors[destinationX][i] != NONE)
-						return false;
-				}
-			}
-			else if (sourceY == destinationY)
-			{
-				xIncrement = (destinationX - sourceX) / (abs(destinationX - sourceX));
-				for (int i = sourceX + xIncrement; i != destinationX; i += xIncrement)
-				{
-					if (fieldsColors[i][destinationY] != NONE)
-						return false;
-				}
-			}
-			else if (abs(sourceX - destinationX) == abs(sourceY - destinationY))
-			{
-				xIncrement = (destinationX - sourceX) / (abs(destinationX - sourceX));
-				yIncrement = (destinationY - sourceY) / (abs(destinationY - sourceY));
-				for (int i = 1; i < abs(sourceX - destinationX); i++)
-				{
-					std::cout << "It got here somehow";
-					if (fieldsColors[sourceX + xIncrement * i][sourceY + yIncrement * i] != NONE)
-						return false;
-				}
-			}
-			else
-				return false;
-
+			return true;
 		}
-		return true;
+		return false;
 	}
 
 	virtual bool isKing()
